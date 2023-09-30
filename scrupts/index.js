8b18d8a6e4b103fe21f5d70e926df161ab51a5fc
SuverBoi <dmitro.kovalik12345@gmail.com>
2023-09-30 19:49:51 +0300

    d

---
+++ scrupts/index.js
@@ -0,0 +1,33 @@
+// 1. 
+// let start = parseInt(prompt("Введіть початкове число діапазону: "));
+// let end = parseInt(prompt("Введіть кінцеве число діапазону: "));
+// let sum = 0;
+
+// for (let i = start; i <= end; sum += i, i++);
+
+// alert(sum); 
+
+// 2.
+// let start = parseInt(prompt("Введіть перше число: "));
+// let end = parseInt(prompt("Введіть дурге число: "));
+// let dilnik = 0;
+
+// тут незнаю як
+
+// 3.
+// let num = parseInt(prompt("Введіть число: "));
+// let arr = [];
+
+// for (let i = 1; i <= num; i++) {
+//     if (num % i === 0) {
+//         arr.push(i);
+//     }
+// }
+
+// alert(arr);
+
+// 4.
+let num = parseInt(prompt("Введіть число: "));
+let numString = num.toString();
+let length = numString.length;
+alert(length)
